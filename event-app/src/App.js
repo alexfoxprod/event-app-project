98bce537b48795c81a062c06e4c3565d3a427bf5
Oleksandra <90516748+OleksandraTurik@users.noreply.github.com>
2022-05-18 15:14:56 +0300

    add routes

--- event-app/src/App.js
+++ event-app/src/App.js
@@ -1,17 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import LoginPage from "./pages/manager/login";
+import RecoveryPage from "./pages/manager/passwordRecovery";
+import Profile from "./pages/user/profile";
+import MembersPage from "./pages/manager/membersManagement";
+
 import "./App.css";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <div className="wrapper">
-        <div className="container">
-          <div className="row">
-            <div className="col-6"></div>
-            <div className="col-6"></div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/recovery" element={<RecoveryPage />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/members" element={<MembersPage />} />
+
+      </Routes>
+    </>
   );
 }
 
